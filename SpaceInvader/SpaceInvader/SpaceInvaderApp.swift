f0244c3b0cc654d670a47a781f5900276a063ec9
Nick Podratz <nick.podratz@syntax-institut.de>
2024-08-05 13:32:41 +0200

    Set up project in Xcode

---
+++ SpaceInvader/SpaceInvader/SpaceInvaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceInvaderApp.swift
+//  SpaceInvader
+//
+//  Created by Syntax on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct SpaceInvaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
